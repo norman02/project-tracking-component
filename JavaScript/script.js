7128a311b86b616d6c52b99df00461154750cd1e
John N. Norman <normanj777@gmail.com>
2020-08-09 11:06:39 -0400

    add toggle function for mobile menue

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,18 +5,44 @@
     4. If a link is slected from the menu revert to initial state.*/
 
 // get elements
+const menu = document.getElementById('menu')
 const menuButton = document.getElementById('menu_button')
 const links = document.getElementsByClassName('menu_link')
 
+let isOpen = false
+
 //open the menu
 const openMenue = ()=> {
-
+    menu.style.display = 'block'
 }
 
 // close the menu
 const closeMenue = ()=> {
-
+    menu.style.display = 'none'
 }
+
+const toggle = ()=> {
+    
+    
+    if (!isOpen) {
+        openMenue()
+        isOpen = true
+    }
+    else {
+        closeMenue()
+        isOpen = false
+    }
+    
+}
+
+menuButton.addEventListener('click', e=> {
+    e.preventDefault
+    toggle()
+} )
+
+// FIXME: 
+document.getElementById('test').addEventListener('click', closeMenue)
+
 
 
 
