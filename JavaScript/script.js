3d572792b0ff06ae7d8040bfcca8a4b3e47f2bb8
John N. Norman <normanj777@gmail.com>
2020-08-09 09:34:26 -0400

    outline js requirement add necessary ids

---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+/* Requirments:  
+    1. Initial state should display the menu button and no menue.
+    2. The menu button should change to the close button when clicked, and the menu should be displayed.
+    3. When the closed button is clicked revert to initial state 
+    4. If a link is slected from the menu revert to initial state.*/
+
+// get elements
+const menuButton = document.getElementById('menu_button')
+const links = document.getElementsByClassName('menu_link')
+
+//open the menu
+const openMenue = ()=> {
+
+}
+
+// close the menu
+const closeMenue = ()=> {
+
+}
+
+
+
